d3c891577fd86a1123cba882ece1024d66cf4c0f
Arron Johnson <arronjohnson.uk@gmail.com>
2022-07-21 13:46:31 +0100

    Add function to fill grid cells when clicked

--- js/script.js
+++ js/script.js
@@ -4,8 +4,9 @@ const gameboard = (() => {
   // create a 3x3 two-dimensional array
   for (let i = 0; i < 3; i++) {
     grid[i] = [];
+
     for (let j = 0; j < 3; j++) {
-      grid[i][j] = "X";
+      grid[i][j] = "";
     }
   }
 
@@ -15,15 +16,28 @@ const gameboard = (() => {
     for (row in grid) {
       const rowElement = document.createElement("div");
       rowElement.className = "grid-row";
+
       for (column in grid[row]) {
         const cellElement = document.createElement("div");
         cellElement.className = "grid-cell";
         cellElement.setAttribute("data-row", row);
         cellElement.setAttribute("data-column", column);
+        cellElement.addEventListener("click", (e) => fillCell(e.target));
         rowElement.appendChild(cellElement);
       }
       container.appendChild(rowElement);
     }
+
+    const fillCell = (element) => {
+      const row = element.getAttribute("data-row");
+      const column = element.getAttribute("data-column");
+
+      if (grid[row][column] !== "") return;
+
+      element.textContent = "X";
+      grid[row][column] = "X";
+      console.log(`Set ${row} ${column} to X`);
+    };
   };
 
   return { drawGrid };
