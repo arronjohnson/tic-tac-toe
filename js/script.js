9957498b86a9be5a6c8a0afebeeae3e3619d94f6
Arron Johnson <arronjohnson.uk@gmail.com>
2022-07-21 16:04:25 +0100

    Implement full game logic
    After a cell has been filled, all cells in the grid are checked for
    the various win conditions. I.e. rows, columns, diagonals. The turn is
    then switched to the other player and the next cell clicked will be
    filled with their marker.
    
    In the event that the board is completely filled without a valid winner,
    a tie is declared.
    
    Currently, the winner is output to the console. This needs to be
    updated to provide an appropriate UI/display.

--- js/script.js
+++ js/script.js
@@ -1,43 +1,93 @@
 const gameboard = (() => {
-  const grid = [];
+  const _grid = [];
 
   // create a 3x3 two-dimensional array
   for (let i = 0; i < 3; i++) {
-    grid[i] = [];
+    _grid[i] = [];
 
     for (let j = 0; j < 3; j++) {
-      grid[i][j] = "";
+      _grid[i][j] = "";
     }
   }
 
   const drawGrid = () => {
     const container = document.querySelector(".grid-container");
 
-    for (row in grid) {
+    for (row in _grid) {
       const rowElement = document.createElement("div");
       rowElement.className = "grid-row";
 
-      for (column in grid[row]) {
+      for (column in _grid[row]) {
         const cellElement = document.createElement("div");
         cellElement.className = "grid-cell";
         cellElement.setAttribute("data-row", row);
         cellElement.setAttribute("data-column", column);
-        cellElement.addEventListener("click", (e) => fillCell(e.target));
+        cellElement.addEventListener("click", (e) => _fillCell(e.target));
         rowElement.appendChild(cellElement);
       }
       container.appendChild(rowElement);
     }
   };
 
-  const fillCell = (element) => {
+  const _fillCell = (element) => {
     const row = element.getAttribute("data-row");
     const column = element.getAttribute("data-column");
 
-    if (grid[row][column] !== "") return;
+    if (game.isFinished() || _grid[row][column] !== "") {
+      return;
+    }
 
-    element.textContent = "X";
-    grid[row][column] = "X";
-    console.log(`Set ${row} ${column} to X`);
+    element.textContent = game.getCurrentPlayer().marker;
+    _grid[row][column] = game.getCurrentPlayer();
+    game.switchTurn();
+    checkGrid();
+  };
+
+  const checkGrid = () => {
+    _checkRows();
+    _checkColumns();
+    _checkDiagonals();
+    _checkFilled();
+  };
+
+  const _checkCells = (cells) => {
+    const player = cells[0];
+    if (!!player && cells.every((c) => c === player)) {
+      game.setWinner(player);
+    }
+  };
+
+  const _checkRows = () => {
+    for (row in _grid) {
+      _checkCells(_grid[row]);
+    }
+  };
+
+  const _checkColumns = () => {
+    for (row in _grid) {
+      const cells = [];
+      for (column in _grid[row]) {
+        cells.push(_grid[column][row]);
+      }
+      _checkCells(cells);
+    }
+  };
+
+  const _checkDiagonals = () => {
+    const mainCells = []; // main diagonal
+    const minorCells = []; // minor diagonal
+    for (row in _grid) {
+      mainCells.push(_grid[row][row]);
+      minorCells.push(_grid[row][3 - 1 - row]);
+    }
+    _checkCells(mainCells);
+    _checkCells(minorCells);
+  };
+
+  const _checkFilled = () => {
+    if (!_grid.flat().includes("")) {
+      game.setWinner("tie");
+    }
   };
 
   return { drawGrid };
@@ -48,25 +98,47 @@ const Player = (marker) => {
 };
 
 const game = (() => {
-  let currentTurn;
+  let _currentPlayer;
+  let _winner;
 
   const player1 = Player("X");
   const player2 = Player("O");
 
   const start = () => {
-    obj.currentTurn = player1;
+    _currentPlayer = player1;
+    _winner = null;
+  };
+
+  const getCurrentPlayer = () => {
+    return _currentPlayer;
   };
 
   const switchTurn = () => {
-    obj.currentTurn = obj.currentTurn === player1 ? player2 : player1;
+    _currentPlayer = _currentPlayer === player1 ? player2 : player1;
   };
 
-  const obj = {
-    currentTurn,
-    start,
-    switchTurn,
+  const setWinner = (winner) => {
+    if (isFinished()) return;
+
+    _winner = winner;
+    if (_winner === "tie") {
+      console.log("It's a tie!");
+    } else {
+      console.log(`${winner.marker} is the winner!`);
+    }
   };
-  return obj;
+
+  const isFinished = () => {
+    return _winner !== null;
+  };
+
+  return {
+    start,
+    getCurrentPlayer,
+    switchTurn,
+    setWinner,
+    isFinished,
+  };
 })();
 
 gameboard.drawGrid();
